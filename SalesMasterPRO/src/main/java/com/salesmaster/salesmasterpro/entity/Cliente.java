4cff36326494ce2b2cb96f70709656c4b0f7a418
G34A4D0 <gerar.210804@gmail.com>
2025-12-01 09:10:02 -0400

    Initial commit

---
+++ SalesMasterPRO/src/main/java/com/salesmaster/salesmasterpro/entity/Cliente.java
@@ -0,0 +1,37 @@
+package com.salesmaster.salesmasterpro.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cliente", schema = "salesmaster")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Long idCliente;
+
+    @Column(name = "nombre", nullable = false, length = 80)
+    private String nombre;
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "telefono", length = 8)
+    private String telefono;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    @Builder.Default
+    private List<Pedido> pedidos = new ArrayList<>();
+}
